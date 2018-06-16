e8c0b1fd5a11a99b831fc0cfb777abd1e158b355
Tom Bilbe <tbilbe@gmail.com>
2018-06-16 11:21:35 +0100

    added img and removed default css custom styling and moved content about in card

--- recipe-props-starter/src/Recipe.js
+++ recipe-props-starter/src/Recipe.js
@@ -3,15 +3,21 @@ import React, { Component } from 'react';
 
 class Recipe extends Component {
   render() {
-    const { title } = this.props;
+    const { title, img, instructions } = this.props;
     const ingredients = this.props.ingredients.map((ingredient, index) => {
       return <li key={index}>{ingredient}</li>
     });
     return (
-      <div>
-        <div>
-          Recipe {title}
+      <div className="recipe-card">
+        <div className="recipe-img">
+          <img src={img} alt={title}></img>
+        </div>
+        <div className="recipe-text">
+          <h3 className="recipe-title">{title}</h3>
+          <h4>Ingredients:</h4>
           <ul>{ingredients}</ul>
+          <h4>Instructions:</h4>
+          <p>{instructions}</p>
         </div>
       </div>
     );
--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
-import logo from './logo.svg';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-        <Recipe title="Pasta" ingredients={["Flour", "Water"]} />
+        <Recipe
+          title="Pasta"
+          ingredients={["Flour", "Water"]}
+          instructions="Add the flour to water and mix"
+          img="spaghetti.jpg"
+        />
       </div>
     );
   }
